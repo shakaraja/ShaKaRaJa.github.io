0048a1a838b8ff76e67571ba0ea118c5acc1de19
Your Namekaitlin <lkaitlin@gmail.com>
2021-12-17 01:09:36 -0600

    added a button to change a gif to a pic

--- main.js
+++ main.js
@@ -16,13 +16,19 @@ function changeColor(item) {
 		item.style.backgroundColor = "pink";
 		this.changed = false
 	}
-	
-}
 
+}
 function letterPressed(letter) {
 	console.log('LETTER:', letter);
 }
+// turns my gif into a gingerbread man
+function ginger() {
+	document.getElementById('ginger').src = 'https://tinyurl.com/2pddh37d';
+	document.getElementById('ginger').width = '280';
+	document.getElementById('ginger').height = '440'
 
+
+}
 // function that tells you whether the letter is in the phrase
 
 // function that tells you whether the letter was already guessed
